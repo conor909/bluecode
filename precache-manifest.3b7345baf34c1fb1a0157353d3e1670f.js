ab80e0135850596f561cb23c88e3bd9b0c192b18
Conor <conor909@gmail.com>
2019-05-02 20:52:26 +0100

    Updates

---
+++ precache-manifest.3b7345baf34c1fb1a0157353d3e1670f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a086b9c423ac2c0e1ea6f290c431590f",
+    "url": "/bluecode/index.html"
+  },
+  {
+    "revision": "ad8fc0ed1bfa1dc3f37e",
+    "url": "/bluecode/static/css/main.a98f2f72.chunk.css"
+  },
+  {
+    "revision": "a707b88805682cb55df5",
+    "url": "/bluecode/static/js/2.de944021.chunk.js"
+  },
+  {
+    "revision": "ad8fc0ed1bfa1dc3f37e",
+    "url": "/bluecode/static/js/main.86cf5caf.chunk.js"
+  },
+  {
+    "revision": "2321cc9be0f4887acda3",
+    "url": "/bluecode/static/js/runtime~main.f5b65a31.js"
+  },
+  {
+    "revision": "1d66cf428589c4db78b64ee58f92fd64",
+    "url": "/bluecode/static/media/logo.1d66cf42.svg"
+  }
+]);
